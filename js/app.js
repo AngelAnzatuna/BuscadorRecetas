4da4b16ef0dcef9173260ef01a6aad86d74650b8
AngelAnzatuna <angelanzatuna.dev@gmail.com>
2025-12-28 15:25:06 +0100

    Guarda recetas favoritas en LocalStorage

--- js/app.js
+++ js/app.js
@@ -138,6 +138,15 @@ function iniciarApp() {
         btnFavorito.classList.add('btn', 'btn-danger', 'col');
         btnFavorito.textContent = 'Guardar Favorito';
 
+        // localStorage
+        btnFavorito.onclick = function() {
+            agregarFavorito({
+                id: idMeal,
+                titulo: strMeal,
+                img: strMealThumb
+            });
+        }
+
         const btnCerrarModal = document.createElement('button');
         btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
         btnCerrarModal.textContent = 'Cerrar';
@@ -150,6 +159,11 @@ function iniciarApp() {
 
         // Muestra el modal
         modal.show();
+    }
+
+    function agregarFavorito(receta) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        localStorage.setItem('favoritos', JSON.stringify([...favoritos, receta]));
     }
 
     function limpiarHTML(selector) {
